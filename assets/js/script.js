19f86c7cbefd0997905f37550b25d0a9c6731f8e
joe-white09 <joe_white09@yahoo.com>
2022-05-05 15:21:08 -0500

    created save tasks function

--- assets/js/script.js
+++ assets/js/script.js
@@ -54,6 +54,7 @@ var createTaskEl = function(taskDataObj) {
   taskDataObj.id = taskIdCounter;
 
   tasks.push(taskDataObj);
+  saveTasks();
 
   // create task actions (buttons and select) for task
   var taskActionsEl = createTaskActions(taskIdCounter);
@@ -118,6 +119,7 @@ var completeEditTask = function(taskName, taskType, taskId) {
       tasks[i].type = taskType;
     }
   };
+  saveTasks();
 
   alert("Task Updated!");
 
@@ -167,7 +169,8 @@ var taskStatusChangeHandler = function(event) {
       tasks[i].status = statusValue;
     }
   }
-  console.log(tasks);
+
+  saveTasks();
 };
 
 var editTask = function(taskId) {
@@ -212,6 +215,12 @@ var deleteTask = function(taskId) {
 
   //reassign tasks array to be the same as updatedTaskArr
   tasks = updatedTaskArr;
+
+  saveTasks();
+};
+
+var saveTasks = function(){
+  localStorage.setItem("tasks", JSON.stringify(tasks));
 };
 
 // Create a new task
